278298ed0c5c079ad80e35dca39ef0bef81bd39e
dineshr3188 <38887639+dineshr3188@users.noreply.github.com>
2022-07-06 22:45:05 +0530

    Add files via upload

--- application/src/main/java/com/blv/tracker/application/config/SecurityConfig.java
+++ application/src/main/java/com/blv/tracker/application/config/SecurityConfig.java
@@ -4,25 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.blv.tracker.application.security.CustomuserDetailsService;
+import com.blv.tracker.application.security.JWTAuthenticationFilter;
+import com.blv.tracker.application.security.JwtAuthenticationEntryPoint;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@SuppressWarnings("deprecation")
-public class SecurityConfig  extends WebSecurityConfigurerAdapter{
+public class SecurityConfig  {
 
 	@Autowired
 	private CustomuserDetailsService userDetailsService;
+	
+	 @Autowired
+	    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+	    @Bean
+	    public JWTAuthenticationFilter jwtAuthenticationFilter(){
+	        return  new JWTAuthenticationFilter();
+	    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -30,17 +42,34 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 		
 	}
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		
-		http.csrf().disable().authorizeRequests()
-		.antMatchers("/api/**").hasAuthority("ADMIN")
-//		.antMatchers("/task/").hasAnyAuthority("SITEENGINEER")
-		.anyRequest().authenticated().and().httpBasic();
-	}
+	@Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests((authorize) -> authorize
+                        .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .antMatchers("/api/auth/**").permitAll()
+                        .antMatchers("/v2/api-docs/**").permitAll()
+                        .antMatchers("/swagger-ui/**").permitAll()
+                        .antMatchers("/swagger-resources/**").permitAll()
+                        .antMatchers("/swagger-ui.html").permitAll()
+                        .antMatchers("/webjars/**").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                );
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-	}
+	@Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
---
+++ application/src/main/java/com/blv/tracker/application/dto/LoginDto.java
@@ -0,0 +1,12 @@
+package com.blv.tracker.application.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+	
+	private String usernameOrEmail;
+	private String password;
+	
+
+}
